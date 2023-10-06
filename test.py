ce05e9c67285ccf920e3bf390873be4e471d7b5c
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-10-06 10:22:49 +0700

    u

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import numpy as np
 
 data = np.random.uniform(1,10,(2,4))
 print(data)
+print(data.shape) # (row,column)
+print(data.size)  # num of elements
+print(data.ravel())
+print("-------------------------------------------")
+data2 = data.reshape(4,2)
+print(data2)
+print(data2[0][1])
+print(data2[1,:])
+print(data2[:,1])
+# data3 = data.reshape(3,3)
+# print(data3)
