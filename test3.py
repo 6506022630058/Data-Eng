87603de3a3afd11ecaf52717dcaf4eeb8165a203
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-10-06 11:16:27 +0700

    u

--- test3.py
+++ test3.py
@@ -9,3 +9,5 @@ print(arr[[3,2,5]]) # row 3 , row 2 , row 5
 arr1 = np.arange(24).reshape((6,4))
 print(arr1)
 print(arr1[[3,2,5],[2,0,1]])  # (row, column)  ->  (3,2) , (2,0) , (5,1)
+
+# axis 0 : row , axis 1 : column
